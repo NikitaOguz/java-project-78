a7971ae56bbca3a923ebbb0ca2a816dbe3444ef8
Никита Кобытев <nikitakobytev@mail.ru>
2026-04-10 00:03:31 +0700

    Fix errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,27 +7,27 @@ plugins {
     id("checkstyle")
     id("io.freefair.lombok") version "8.6"
     id("com.github.ben-manes.versions") version "0.50.0"
-    id("com.gradleup.shadow") version "9.2.2"
-    // Плагин для публикации отчета о покрытии тестами на SonarQube
     id("org.sonarqube") version "7.2.3.7755"
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
-application { mainClass.set("hexlet.code.App") }
+application {
+    mainClass.set("hexlet.code.App")
+}
 
-repositories { mavenCentral() }
+repositories {
+    mavenCentral()
+}
 
 dependencies {
-    // Основные библиотеки проекта
     implementation("org.apache.commons:commons-lang3:3.14.0")
     implementation("org.apache.commons:commons-collections4:4.4")
     implementation("info.picocli:picocli:4.7.7")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.17.0")
 
-    // JUnit 5
     testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.1")
@@ -42,15 +42,21 @@ tasks.test {
     }
 }
 
-tasks.jacocoTestReport { reports { xml.required.set(true) } }
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+    }
+}
 
 sonar {
     properties {
-        property ("sonar.projectKey", "NikitaOguz_java-project-78")
-        property ("sonar.organization", "nikitoguzkov")
+        property("sonar.projectKey", "NikitaOguz_java-project-78")
+        property("sonar.organization", "nikitoguzkov")
     }
 }
+
 tasks.register("install") {
     dependsOn("installDist")
 }
+
 
