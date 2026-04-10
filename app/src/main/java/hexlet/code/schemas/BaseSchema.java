1aacd3f44501853b4c8d5b519d85baaa7c7e5ff8
Никита Кобытев <nikitakobytev@mail.ru>
2026-04-10 15:43:20 +0700

    Fix errors

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -7,23 +7,26 @@ import java.util.function.Predicate;
 public abstract class BaseSchema<T> {
     protected Map<String, Predicate<T>> check = new LinkedHashMap<>();
     protected boolean isRequired;
+
     public abstract BaseSchema<T> required();
 
     public final void setRequired() {
         isRequired = true;
     }
+
     public final boolean isValid(T data) {
         if (data == null) {
             return !isRequired;
         }
+
         for (Map.Entry<String, Predicate<T>> valid : check.entrySet()) {
             if (!valid.getValue().test(data)) {
                 return false;
             }
-
         }
         return true;
     }
+
     protected final void addCheck(String toAddName, Predicate<T> toAdd) {
         check.put(toAddName, toAdd);
     }
--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -3,20 +3,20 @@ package hexlet.code.schemas;
 import java.util.Map;
 
 public final class MapSchema<T> extends BaseSchema<Map<String, T>> {
+
+    @Override
     public MapSchema<T> required() {
         setRequired();
         return this;
     }
+
     public MapSchema<T> sizeof(int size) {
         addCheck("sizeof", value -> value.size() == size);
         return this;
     }
+
     public MapSchema<T> shape(Map<String, BaseSchema<T>> shapeMap) {
         addCheck("shape", value -> {
-            if (value == null) {
-                return !isRequired;
-            }
-
             for (var entry : shapeMap.entrySet()) {
                 String key = entry.getKey();
                 BaseSchema<T> schema = entry.getValue();
@@ -26,15 +26,8 @@ public final class MapSchema<T> extends BaseSchema<Map<String, T>> {
                     return false;
                 }
             }
-
             return true;
         });
         return this;
     }
-
-    @SuppressWarnings("unchecked")
-    private boolean checkBySchema(BaseSchema<?> schema, Object value) {
-        return ((BaseSchema<Object>) schema).isValid(value);
-    }
 }
-
