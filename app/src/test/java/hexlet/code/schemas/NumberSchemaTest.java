27a763509326a7bff74fb1f46732397c871bdf97
Никита Кобытев <nikitakobytev@mail.ru>
2026-04-10 23:26:46 +0700

    Fix errors

--- app/src/test/java/hexlet/code/schemas/NumberSchemaTest.java
+++ app/src/test/java/hexlet/code/schemas/NumberSchemaTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class NumberSchemaTest {
+public final class NumberSchemaTest {
     private NumberSchema numberSchema;
     @BeforeEach
     public void setUp() {
--- app/src/test/java/hexlet/code/schemas/StringSchemaTest.java
+++ app/src/test/java/hexlet/code/schemas/StringSchemaTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class StringSchemaTest {
+public final class StringSchemaTest {
     private StringSchema stringSchema;
 
     @BeforeEach
