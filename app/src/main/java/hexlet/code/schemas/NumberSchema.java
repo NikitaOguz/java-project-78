e06748b61d3f19e56f8f60617f8d10dc12fd5939
NikitOguz <140327196+NikitaOguz@users.noreply.github.com>
2026-04-14 00:11:29 +0700

    Update NumberSchema.java

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -8,22 +8,13 @@ public final class NumberSchema extends BaseSchema<Integer> {
     }
 
     public NumberSchema positive() {
-        addCheck("positive", value -> {
-            if (value == null) {
-                return !isRequired;
-            }
-            return value > 0;
-        });
+        addCheck("positive", value -> value == null || value > 0);
         return this;
     }
 
     public NumberSchema range(int start, int end) {
-        addCheck("range", value -> {
-            if (value == null) {
-                return !isRequired;
-            }
-            return value >= start && value <= end;
-        });
+        addCheck("range", value -> value == null || (value >= start && value <= end));
         return this;
     }
 }
+
